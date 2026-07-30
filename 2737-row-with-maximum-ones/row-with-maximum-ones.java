6c4265e6659baa6e42a17d1d4d7fa26add25cc39
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-07-30 21:07:36 +0530

    Time: 11 ms (7.96%) | Memory: 49.2 MB (5.33%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -2,18 +2,35 @@ class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
+        for (int i = 0; i < n; i++) {
+            Arrays.sort(mat[i]);   // sort each row
+        }
         int maxcount=0;
         int index =0;
+        int x=1;
         for(int i=0;i<n;i++){
             int rowcount=0;
-            for(int j=0;j<m;j++){
-                rowcount+=mat[i][j];
-                if(rowcount>maxcount){
-                    maxcount=rowcount;
-                    index=i;
-                }
+            rowcount=m-maxones(mat[i],m,x);
+            if(rowcount>maxcount){
+                maxcount=rowcount;
+                index=i;
             }
         }
         return new int[]{index,maxcount};
+    }
+    public int maxones(int[] mat , int n , int x){
+        int low =0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(mat[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 }
