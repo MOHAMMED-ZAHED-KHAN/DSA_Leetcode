2b02eb5021590711b90a13e013e5e02aaf032911
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-07-31 17:05:25 +0530

    Time: 1 ms (100.00%) | Memory: 48.3 MB (55.80%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,51 +1,19 @@
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
+        int n =mat.length;
+        int m =mat[0].length;
         int maxcount=0;
         int index =0;
-
-        // Since the array is not sorted to using binary search is actually slower than O(n^2) ..
-        // if the questions mentions that array is sorted then we will use Binary search approach..
-
-        // int x=1;
-        // for (int i = 0; i < n; i++) {
-        //     Arrays.sort(mat[i]);   // sort each row
-        // }
-        // for(int i=0;i<n;i++){
-        //     int rowcount=0;
-        //     rowcount=m-maxones(mat[i],m,x);
-        //     if(rowcount>maxcount){
-        //         maxcount=rowcount;
-        //         index=i;
-        //     }
-        // }
-        
         for(int i=0;i<n;i++){
-            int rowcount=0;
+            int rowwisecount=0;
             for(int j=0;j<m;j++){
-                rowcount+=mat[i][j];
-                if(rowcount>maxcount){
-                    maxcount=rowcount;
+                rowwisecount+=mat[i][j];
+                if(rowwisecount>maxcount){
+                    maxcount=rowwisecount;
                     index=i;
                 }
             }
         }
-        return new int[]{index,maxcount};
+        return new int[] {index,maxcount};
     }
-    // public int maxones(int[] mat , int n , int x){
-    //     int low =0;
-    //     int high=n-1;
-    //     int ans=n;
-    //     while(low<=high){
-    //         int mid = low+(high-low)/2;
-    //         if(mat[mid]>=x){
-    //             ans=mid;
-    //             high=mid-1;
-    //         }else{
-    //             low=mid+1;
-    //         }
-    //     }
-    //     return ans;
-    // }
 }
