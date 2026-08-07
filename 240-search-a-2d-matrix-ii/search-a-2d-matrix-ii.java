8a104ba6665b0f39daac9e0845f4a0b5f3ec34f1
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-08-07 20:15:30 +0530

    Time: 3 ms (88.44%) | Memory: 47.8 MB (96.89%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n =  matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+                if (findtarget(matrix[i], target)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean findtarget(int[] matrix , int target){
+        int n = matrix.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(matrix[mid]==target){
+                return true;
+            }else if (target > matrix[mid]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
