8fe118e59c45f3768650c9f227618d76d53904bf
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-08-15 14:23:02 +0530

    Time: 2 ms (42.57%) | Memory: 94.3 MB (71.61%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int minprice=prices[0];
+        int maxprofit=0;
+        for(int i=1;i<n;i++){
+            minprice=Math.min(minprice,prices[i]);
+            maxprofit=Math.max(maxprofit,prices[i]-minprice);
+        }
+        return maxprofit;
+    }
+}
