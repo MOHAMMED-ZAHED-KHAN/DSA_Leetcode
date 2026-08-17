89ce3dad19b68f31b856624ad72876ebee40ad38
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-08-17 21:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.52%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n =nums.length;
+        int ls=0;
+        int sum=0;
+        for(int i : nums){
+            sum+=i;
+        }
+        for(int i=0;i<n;i++){
+            int rs = sum-ls-nums[i];
+            if(ls==rs){
+                return i;
+            }
+            ls+=nums[i];
+        }
+        return -1;
+    }
+}
