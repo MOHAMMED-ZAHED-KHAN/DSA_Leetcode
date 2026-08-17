b5a529cf7ac9c7bf794ae2ce7694af74300e2447
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-08-17 21:49:21 +0530

    Time: 4 ms (99.35%) | Memory: 159.7 MB (77.38%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,32 +1,22 @@
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
-        ArrayList<Integer> less = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
-        ArrayList<Integer> greater = new ArrayList<>();
-        for(int i=0;i<n;i++){
+    public static int[] pivotArray(int[] nums, int pivot) {
+        int[] ans=new int[nums.length];
+        int ind=0;
+        for(int i=0;i<nums.length;i++){
             if(nums[i]<pivot){
-                less.add(nums[i]);
-            }else if(nums[i]==pivot){
-                equal.add(nums[i]);
-            }else{
-                greater.add(nums[i]);
+                ans[ind++]=nums[i];
             }
-        }
-        int[] ans = new int[n];
-        int index=0;
-        for(int i=0;i<less.size();i++){
-            ans[index]=less.get(i);
-            index++;
-        }
-        for(int i=0;i<equal.size();i++){
-            ans[index]=equal.get(i);
-            index++;
-        }
-        for(int i=0;i<greater.size();i++){
-            ans[index]=greater.get(i);
-            index++;
-        }
+        } 
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==pivot){
+                ans[ind++]=nums[i];
+            }
+        } 
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>pivot){
+                ans[ind++]=nums[i];
+            }
+        } 
         return ans;
     }
 }
